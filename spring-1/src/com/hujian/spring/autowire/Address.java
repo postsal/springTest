a1194704649d41e9943d8edd51a06dd6dda0e378
胡建 <1072841098@qq.com>
2016-10-08 20:05:45 +0800

    spring自动装配bean

---
+++ spring-1/src/com/hujian/spring/autowire/Address.java
@@ -0,0 +1,33 @@
+package com.hujian.spring.autowire;
+
+/**
+ * Created by hujian on 2016/10/8.
+ */
+public class Address {
+    private String city;
+    private String street;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                '}';
+    }
+}

0c87611bb4ebbe0939ea596fb15497c5531f8670
胡建 <1072841098@qq.com>
2016-10-10 14:30:54 +0800

    通过FactoryBean来配置Bean实例

--- spring-1/src/com/hujian/spring/factorybean/CarFactoryBean.java
+++ spring-1/src/com/hujian/spring/factorybean/CarFactoryBean.java
@@ -3,6 +3,7 @@ package com.hujian.spring.factorybean;
 import org.springframework.beans.factory.FactoryBean;
 
 /**
+ * 自定义的FactoryBean需要实现Spring提供的FactoryBean接口
  * Created by hujian on 2016/10/9.
  */
 public class CarFactoryBean implements FactoryBean<Car> {
